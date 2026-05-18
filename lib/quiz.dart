7cc2459d0142c50d742ad65f063ff75abda36fec
Al Arafat <hossainarafat071@gmail.com>
2026-05-19 03:20:41 +0600

    update DevQuiz project

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,6 @@
+import 'package:devquiz/question_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:devquiz/homepage.dart';
-
 
 var startAl = Alignment.topLeft;
 var endAl = Alignment.bottomRight;
@@ -15,23 +15,31 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  
+  Widget activeScreen = Homepage();
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-          Color.fromARGB(255, 65, 67, 204),
-          Color.fromARGB(255, 8, 9, 44),
-        ], begin: startAl, end: endAl),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 65, 67, 204),
+                Color.fromARGB(255, 8, 9, 44),
+              ],
+              begin: startAl,
+              end: endAl,
+            ),
+          ),
+          child: activeScreen,
+        ),
       ),
-      child: const Homepage(),
-    ),
-    ),
     );
   }
 }
