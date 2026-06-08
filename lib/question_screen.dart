99117964133ff1914fc2acd2052a08689cdba72a
Al Arafat <hossainarafat071@gmail.com>
2026-06-08 20:57:06 +0600

    update quiz screen logic

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 
-class QuestionScreen extends StatefulWidget{
+class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
 
   @override
-  State<QuestionScreen> createState(){
-  return _QuestionScreen();
+  State<QuestionScreen> createState() {
+    return _QuestionScreen();
   }
 }
 
-class _QuestionScreen extends State<QuestionScreen>{
-
-@override
- Widget build(BuildContext context){
-    return Text(
-      'Question Screen'
+class _QuestionScreen extends State<QuestionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text("The question is...."),
+        const SizedBox(height: 30),
+        ElevatedButton(onPressed: () {}, child: const Text("Answer 1")),
+      ],
     );
- }
-
-
+  }
 }
